0e01a35bd0fec222e7f18e5763ff98aa6a451972
Liuyuyuan74 <95260740+Liuyuyuan74@users.noreply.github.com>
2023-11-25 18:38:44 -0500

    change a little

---
+++ AttendanceTrackerNew/app/src/main/java/com/wpi/attendancetracker/Checkin.kt
@@ -0,0 +1,10 @@
+package com.wpi.attendancetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Checkin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
