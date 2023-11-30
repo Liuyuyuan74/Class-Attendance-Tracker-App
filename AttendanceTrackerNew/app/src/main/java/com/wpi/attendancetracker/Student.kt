ef7d74bfa757aa477258d153872ae82a759e7502
Kangjian Wu <wkj0713@gmail.com>
2023-11-30 18:05:29 -0500

    student home page

--- AttendanceTrackerNew/app/src/main/java/com/wpi/attendancetracker/Student.kt
+++ AttendanceTrackerNew/app/src/main/java/com/wpi/attendancetracker/Student.kt
@@ -6,23 +6,38 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import android.util.Log
 
 class Student : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
 
-        val classItems = listOf(ClassItem("Math 101"), ClassItem("Science 202"))
+        val db = FirebaseFirestore.getInstance()
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewClasses)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ClassesAdapter(classItems)
+
+        db.collection("classes")
+            .get()
+            .addOnSuccessListener { documents ->
+                val classItems = documents.mapNotNull { doc ->
+                    doc.getString("className")?.let { name ->
+                        ClassItem(name)
+                    }
+                }
+                recyclerView.adapter = ClassesAdapter(classItems)
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Student", "Error getting documents: ", exception)
+            }
 
         val btnGoToReport = findViewById<Button>(R.id.btnGoToReport)
-
         btnGoToReport.setOnClickListener {
             val reportIntent = Intent(this, ClassReportActivity::class.java)
-            reportIntent.putExtra(ClassReportActivity.CLASS_KEY, "VcFBzK4fFnzD3PZ1OrO8");
-            startActivity(reportIntent);
+            reportIntent.putExtra(ClassReportActivity.CLASS_KEY, "VcFBzK4fFnzD3PZ1OrO8")
+            startActivity(reportIntent)
         }
     }
 }
