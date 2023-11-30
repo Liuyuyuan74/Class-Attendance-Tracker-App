3a0355730592092c97306a707a6d634b49cd2b31
Liuyuyuan74 <95260740+Liuyuyuan74@users.noreply.github.com>
2023-11-30 18:08:04 -0500

    add basic function

--- AttendanceTrackerNew/app/src/main/java/com/wpi/attendancetracker/StudentCheckin.kt
+++ AttendanceTrackerNew/app/src/main/java/com/wpi/attendancetracker/StudentCheckin.kt
@@ -2,20 +2,41 @@ package com.wpi.attendancetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Date
 
 class StudentCheckin : AppCompatActivity() {
 
-    val logClassDetails: (classDetail: DatabaseUtil.ClassDetail?) -> Unit = { classDetail ->
-        if (classDetail != null) {
-            Log.d("className", classDetail.className)
-        }
-    }
+    private lateinit var courseInfoTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_checkin)
+
+        courseInfoTextView = findViewById(R.id.textViewCourseInfo)
+
         val databaseUtil = DatabaseUtil()
-        databaseUtil.getClass("VcFBzK4fFnzD3PZ1OrO8", logClassDetails)
+        databaseUtil.getClass("VcFBzK4fFnzD3PZ1OrO8") { classDetail ->
+            if (classDetail != null) {
+                courseInfoTextView.text = "Class Name: ${classDetail.className}\nOther details here..."
+            }
+        }
+
+        val checkInButton = findViewById<ImageButton>(R.id.imageButton3)
+        checkInButton.setOnClickListener {
+            // Replace 'studentID' and 'classID' with actual values or logic to obtain these values
+            databaseUtil.addCheckIn("studentID", "classID", Date()).addOnSuccessListener {
+                Toast.makeText(this, "Check-in Successful", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Check-in Failed: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val backButton = findViewById<ImageButton>(R.id.imageButton2)
+        backButton.setOnClickListener {
+            finish() // Finish the current activity, returning to the previous one in the stack
+        }
     }
 }
